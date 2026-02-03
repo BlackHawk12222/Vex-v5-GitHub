48d9d15a5c188b6a49d8a573e064d7507a774f38
GreyWolf1244 <Daniel@orangekarat.com>
2026-02-03 15:30:27 -0600

    Update functions.py

--- Initailation/functions.py
+++ Initailation/functions.py
@@ -12,4 +12,9 @@ def stop_l_r_drive_l_r(stop_l_r_drive_l_r__l_r):
             Right3.stop()
         else:
             pass
-#
+#drive reverse; accurate, requires Raw_Drive("r",drivemod)
+def Drive_reverse_dist_milinmeters(Drive_reverse_dist_milinmeters__dist):
+    global my_event, fake_auto, sd_is_in, Accuracy, Front_Down, right_temp, left_temp, Descoring, turn_mod, DegreesToTurn, TurnData, driveMod, auto_side, Auto_color, leftData, RightData, IntakeData, iteration, LeftDriveData, RightDriveData, IntakeDriveData, Left_Iter, Right_Iter, Intake_Iter, textReadout, LeftVP, RightVP, BreakParsing, AuBP_MaxVP, colortoggle, skillsRun, recording, MatchLoadData, TopMotorDATA, TopMotorDriveDATA, MatchLoadDriveDATA, matchload_iter, top_iter, use_turningInertial, LastFront_down, turn_to_h_dif, Kp, Ki, Kd, error, loop_delay, last_error, integral, position, integral_limit, error_threshhold, derivative, POWER, intake_speed, screen_precision, console_precision, ai_vision_2_index, ai_vision_2_objects, controller_1_precision
+    driveMod = (Drive_reverse_dist_milinmeters__dist / ((69.85 * 3.14) / 360)) / 0.76
+    Raw__Drive_f_r_f_r_for_d_degrees("r", driveMod)
+    wait(0.1, SECONDS)

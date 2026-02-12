19a4395ff532a86df107fda78a4779b3128d51af
misquishy3244-hub <misquishy3244@gmail.com>
2026-02-12 13:08:54 -0600

    Power saving addition

---
+++ vs_code/Power Saving code/Power_Saving_v1/src/main.py
@@ -0,0 +1,19 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       MicaS                                                        #
+# 	Created:      2/12/2026, 1:07:05 PM                                        #
+# 	Description:  V5 project                                                   #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+
+# Brain should be defined by default
+brain=Brain()
+
+brain.screen.print("Hello V5")
+
+
+        

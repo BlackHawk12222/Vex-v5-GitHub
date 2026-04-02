3fe4364cecf34ddbbd9e68d8662d40b967fba68a
misquishy3244-hub <misquishy3244@gmail.com>
2026-04-02 12:40:50 -0500

    CLEARpp

--- vs_code/CLEAR code/CLEARpp v.01/include/CLEAR.hpp
+++ vs_code/CLEAR code/CLEARpp v.01/include/CLEAR.hpp
@@ -2,6 +2,7 @@
 #define CLEAR_HPP
 #include "main.h"
 #include <list>
+#include <unordered_map>
 
 class log{
  public:   
@@ -15,7 +16,23 @@ class log{
     std::list<short unsigned int> motor_monitor_current;
     std::list<short unsigned int> motor_monitor_power;
     std::list<short unsigned int> motor_monitor_efficiency;
+    long unsigned int index = 0;
+    std::unordered_map<std::string, std::string> log_codes={
+        {"MD01", "Motor DATA: Temps Normal. Temps: "},
+        {"MD02", "Motor DATA: Power Normal. Power: "},
+        {"MD03", "Motor DATA: Current Normal. Current: "},
+        {"MD04", "Motor DATA: Efficiency Normal. Efficiency: "},
+        {"MW01", "Motor WARNING: Temps High. Temps: "},
+        {"MW02", "Motor WARNING: Power High. Power: "},
+        {"MW03", "Motor WARNING: Current High. Current: "},
+        {"MW04", "Motor WARNING: Efficiency Low. Efficiency: "},
+        {"ME01", "Motor ERROR: Temps Critical. Temps: "},
+        {"ME02", "Motor ERROR: Power Critical. Power: "},
+        {"ME03", "Motor ERROR: Current Critical. Current: "},
+        {"ME04", "Motor ERROR: Efficiency Critical. Efficiency: "},
+    };
     void init_motor_monitor(std::list<pros::Motor> motors);
+    void update_motor_monitor(std::list<pros::Motor> motors);
     log();
 };
 #endif CLEAR_HPP
---
+++ vs_code/CLEAR code/CLEARpp v.01/src/CLEAR.cpp
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <list>
+#include "CLEAR.hpp"
+#include <unordered_map>
+
+void log::add(const char* Code, const char* Details){
+    std::uint32_t time = pros::millis();
+    std::string log_message = "[" + std::to_string(time) + "] " + log_codes[Code] + Details;
+    printf("%s\n", log_message.c_str());
+    FILE* log_file = fopen("/usd/log.txt", "a");
+    if (log_file != nullptr) {
+        fprintf(log_file, "%s\n", log_message.c_str());
+        fclose(log_file);
+    } else {
+        printf("Error opening log file for writing.\n");
+    }
+    fclose(log_file);
+}
+
+void log::init_motor_monitor(std::list<pros::Motor> motors){
+    for (pros::Motor motor : motors){
+            motor_monitor_temp.push_back(motor.get_temperature());
+            motor_monitor_power.push_back(motor.get_power());
+            motor_monitor_current.push_back(motor.get_current_draw());
+            motor_monitor_efficiency.push_back(motor.get_efficiency());
+        
+    }
+}
+
+void log::log_start(std::list<pros::Motor> motors){
+    init_motor_monitor(motors);
+    while (true){
+        std::uint32_t time_start = pros::millis();
+        update_motor_monitor(motors);
+        std::uint32_t time_end = pros::millis();
+        pros::delay(200- (time_end - time_start));
+    }
+}
